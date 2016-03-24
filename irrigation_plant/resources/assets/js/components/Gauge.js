b56f613440b830e027117571feae233d3149f0be
Ingvar Kreft <Ingvar.Kreft@cybercom.com>
2016-03-24 15:36:55 +0100

    Moved create of gauge to front end

--- irrigation_plant/resources/assets/js/components/Gauge.js
+++ irrigation_plant/resources/assets/js/components/Gauge.js
@@ -1,28 +1,13 @@
 import gauge from 'canv-gauge';
 
 export default {
-  template: '<canvas id="{{ gid }}" data-type="canv-gauge" width="200" height="200"></canvas>',
+  template: '<canvas id="gauge_{{ gid }}" ' +
+      'data-type="canv-gauge" ' +
+      'data-highlights="{{ highlights }}"' +
+      'width="150" height="150"></canvas>',
 
-  props: ['gid', 'values'],
+  props: ['gid', 'values', 'highlights'],
 
   ready() {
-  //  var data = {
-  //    labels: this.labels,
-  //    datasets: [
-  //    {
-  //      fillColor: "rgba(220,220,220,0.2)",
-  //      strokeColor: "rgba(220,220,220,1)",
-  //      pointColor: "rgba(220,220,220,1)",
-  //      pointStrokeColor: "#fff",
-  //      pointHighlightFill: "#fff",
-  //      pointHighliteStroke: "rgba(220,220,220,1)",
-  //      data: this.values
-  //    }
-  //    ]
-  //  }
-
-  //  new Chart(
-  //    this.$el.getContext('2d')
-   //   ).context).Line(data);
   }
 }
--- irrigation_plant/resources/assets/js/main.js
+++ irrigation_plant/resources/assets/js/main.js
@@ -6,4 +6,9 @@ new Vue({
   el: 'body',
 
   components: { Graph, Gauge },
+
+  ready() {
+    console.log('ready');
+    //gauge.Collection.get(1).setValue( 54.3);
+  }
 })
